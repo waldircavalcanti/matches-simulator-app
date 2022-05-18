59e6a4b6e35688293c8cf2ea1fb108790ec60283
Waldir Cavalcanti <walcanty22@gmail.com>
2022-05-18 11:37:22 -0300

    Inclusão do Firebase

--- app/src/main/java/me/dio/simulator/ui/DetailActivity.kt
+++ app/src/main/java/me/dio/simulator/ui/DetailActivity.kt
@@ -30,21 +30,21 @@ class DetailActivity : AppCompatActivity() {
     private fun loadMatchFromExtra() {
         intent?.extras?.getParcelable<Match>(Extras.MATCH)?.let {
             Glide.with(this).load(it.place.image).into(binding.ivPlace)
-            supportActionBar?.title =it.place.name
+            supportActionBar?.title = it.place.name
 
             binding.tvDescription.text = it.description
 
             Glide.with(this).load(it.homeTeam.image).into(binding.ivHomeTeam)
             binding.tvHomeTeamName.text = it.homeTeam.name
-            binding.rbHomeTeamStars.rating =it.homeTeam.stars.toFloat()
-            if (it.homeTeam.score != null){
+            binding.rbHomeTeamStars.rating = it.homeTeam.stars.toFloat()
+            if (it.homeTeam.score != null) {
                 binding.tvHomeTeamScore.text = it.homeTeam.score.toString()
             }
 
             Glide.with(this).load(it.awayTeam.image).into(binding.ivAwayTeam)
             binding.tvAwayTeamName.text = it.awayTeam.name
-            binding.rbAwayTeamStars.rating =it.awayTeam.stars.toFloat()
-            if (it.awayTeam.score != null){
+            binding.rbAwayTeamStars.rating = it.awayTeam.stars.toFloat()
+            if (it.awayTeam.score != null) {
                 binding.tvAwayTeamScore.text = it.awayTeam.score.toString()
             }
         }
